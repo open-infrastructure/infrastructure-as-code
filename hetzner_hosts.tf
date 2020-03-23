fe3f47f0a5cac2c4bcbe67d14621ed3e0a204cc5
cedi <cedi@users.noreply.github.com>
2020-03-23 21:52:49 +0100

    rename gateway

--- hetzner_hosts.tf
+++ hetzner_hosts.tf
@@ -1,6 +1,6 @@
 module "dev_www" {
   source = "./modules/terraform-hcloud_azuredns"
-  server_name = "gw1"
+  server_name = "fsn1-gw1"
   ssh_keys = ["cedi@ivy", "cedi@ivy legacy", "azure_pipelines"]
   az_dns_zone = azurerm_dns_zone.vpn_dns
 	environment = var.environment
