28d07c0f9b24156ecfc24f35a51ecce2c6b73727
cedi <cedi@users.noreply.github.com>
2020-03-24 12:48:12 +0100

    add VPN DNS Zone

--- vpn.tf
+++ vpn.tf
@@ -1,8 +1,21 @@
+resource "azurerm_dns_zone" "vpn_oi_dns" {
+  name                = "vpn.open-infrastructure.de"
+  resource_group_name = azurerm_resource_group.openinfrastructure_rg.name
+}
+
+resource "azurerm_dns_ns_record" "vpn" {
+  name                = "vpn"
+  zone_name           = azurerm_dns_zone.openinfrastructure_dns.name
+  resource_group_name = azurerm_resource_group.openinfrastructure_rg.name
+  ttl                 = 3600
+  records = vpn_oi_dns.name_servers
+}
+
 module "dev_www" {
   source = "./modules/terraform-hcloud_azuredns"
   server_name = "fsn1-gw1"
   ssh_keys = ["cedi@ivy", "cedi@ivy legacy", "azure_pipelines"]
-  az_dns_zone = azurerm_dns_zone.openinfrastructure_dns
+  az_dns_zone = azurerm_dns_zone.vpn_oi_dns
 	environment = var.environment
 }
 
