fd8e6dd14487b215ab2486b182bafaf6ba0d1128
cedi <cedi@users.noreply.github.com>
2020-03-24 12:50:36 +0100

    fix

--- vpn.tf
+++ vpn.tf
@@ -8,7 +8,7 @@ resource "azurerm_dns_ns_record" "vpn" {
   zone_name           = azurerm_dns_zone.openinfrastructure_dns.name
   resource_group_name = azurerm_resource_group.openinfrastructure_rg.name
   ttl                 = 3600
-  records = vpn_oi_dns.name_servers
+  records = azurerm_dns_zone.vpn_oi_dns.name_servers
 }
 
 module "dev_www" {
