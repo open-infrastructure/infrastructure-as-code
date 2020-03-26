48138df2b0c0cb1be2609932cf6b58698c415116
cedi <cedi@users.noreply.github.com>
2020-03-26 14:56:51 +0100

    add bbb

---
+++ bigbluebutton.tf
@@ -0,0 +1,15 @@
+resource "azurerm_dns_a_record" "bbb1" {
+  name                = "bbb1"
+  zone_name           = azurerm_dns_zone.openinfrastructure_dns.name
+  resource_group_name = azurerm_resource_group.openinfrastructure_rg.name
+  ttl                 = 3600
+  records             = ["49.12.34.163"]
+}
+
+resource "azurerm_dns_aaaa_record" "bbb1" {
+  name                = "bbb1"
+  zone_name           = azurerm_dns_zone.openinfrastructure_dns.name
+  resource_group_name = azurerm_resource_group.openinfrastructure_rg.name
+  ttl                 = 3600
+  records             = ["2a01:4f8:c17:1ec7::1"]
+}
