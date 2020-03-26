b82455e938f764f32f08e294d59ba59ab9677ad9
Thies Mueller <tmueller@wobcom.de>
2020-03-26 15:01:25 +0100

    added bbb

--- bigbluebutton.tf
+++ bigbluebutton.tf
@@ -1,3 +1,19 @@
+resource "azurerm_dns_a_record" "bbb" {
+  name                = "bbb"
+  zone_name           = azurerm_dns_zone.openinfrastructure_dns.name
+  resource_group_name = azurerm_resource_group.openinfrastructure_rg.name
+  ttl                 = 3600
+  records             = ["176.9.61.37"]
+}
+
+resource "azurerm_dns_aaaa_record" "bbb" {
+  name                = "bbb"
+  zone_name           = azurerm_dns_zone.openinfrastructure_dns.name
+  resource_group_name = azurerm_resource_group.openinfrastructure_rg.name
+  ttl                 = 3600
+  records             = ["2a01:4f8:150:942d::1"]
+}
+
 resource "azurerm_dns_a_record" "bbb1" {
   name                = "bbb1"
   zone_name           = azurerm_dns_zone.openinfrastructure_dns.name
