d78149786a42888b21f1288e01179fa94ed69316
cedi <cedi@users.noreply.github.com>
2020-03-27 11:31:03 +0100

    fix dns zone name

--- zammad.tf
+++ zammad.tf
@@ -2,7 +2,7 @@ module "zammad" {
   source = "./modules/terraform-hcloud_azuredns"
   server_name = "tickets"
   ssh_keys = ["cedi@ivy", "cedi@ivy legacy", "azure_pipelines"]
-  az_dns_zone = azurerm_dns_zone.jitsi_rocks_dns
+  az_dns_zone = azurerm_dns_zone.openinfrastructure_dns
 	environment = var.environment
   server_type = "cx11"
 	image = "ubuntu-18.04"
