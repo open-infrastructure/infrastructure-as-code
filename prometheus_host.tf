27c33046112d6fc9815393d695312affae4a510a
cedi <cedi@users.noreply.github.com>
2020-03-29 22:35:54 +0200

    add second entry for prometheus.open-infrastructure.de

--- prometheus_host.tf
+++ prometheus_host.tf
@@ -1,14 +1,4 @@
-module "grafana" {
-  source = "./modules/terraform-hcloud_azuredns"
-  server_name = "monitor"
-  ssh_keys = ["cedi@ivy", "cedi@ivy legacy", "azure_pipelines"]
-  az_dns_zone = azurerm_dns_zone.openinfrastructure_dns
-	environment = var.environment
-	image = "ubuntu-18.04"
-  server_type="cx11"
-}
-
-resource "azurerm_dns_a_record" "hcloud_host_dns_entry" {
+resource "azurerm_dns_a_record" "prometheus_a_entry" {
   name                = "prometheus"
   zone_name           = azurerm_dns_zone.openinfrastructure_dns.name
   resource_group_name = azurerm_resource_group.openinfrastructure_rg.name
@@ -16,8 +6,24 @@ resource "azurerm_dns_a_record" "hcloud_host_dns_entry" {
   records             = ["78.46.64.157"]
 }
 
-resource "azurerm_dns_aaaa_record" "hcloud_host_dns_entry" {
+resource "azurerm_dns_aaaa_record" "prometheus_aaaa_entry" {
   name                = "prometheus"
+  zone_name           = azurerm_dns_zone.openinfrastructure_dns.name
+  resource_group_name = azurerm_resource_group.openinfrastructure_rg.name
+  ttl                 = 300
+  records             = ["2a01:4f8:120:23b3::2"]
+}
+
+resource "azurerm_dns_a_record" "monitor_a_entry" {
+  name                = "monitor"
+  zone_name           = azurerm_dns_zone.openinfrastructure_dns.name
+  resource_group_name = azurerm_resource_group.openinfrastructure_rg.name
+  ttl                 = 300
+  records             = ["78.46.64.157"]
+}
+
+resource "azurerm_dns_aaaa_record" "monitor_aaaa_entry" {
+  name                = "monitor"
   zone_name           = azurerm_dns_zone.openinfrastructure_dns.name
   resource_group_name = azurerm_resource_group.openinfrastructure_rg.name
   ttl                 = 300
