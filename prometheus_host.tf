3fc8a76ea9764b25222a400ae931caacde68d65c
cedi <cedi@users.noreply.github.com>
2020-03-29 22:03:21 +0200

    add grafana instance

--- prometheus_host.tf
+++ prometheus_host.tf
@@ -1,3 +1,13 @@
+module "grafana" {
+  source = "./modules/terraform-hcloud_azuredns"
+  server_name = "monitor"
+  ssh_keys = ["cedi@ivy", "cedi@ivy legacy", "azure_pipelines"]
+  az_dns_zone = azurerm_dns_zone.openinfrastructure_dns
+	environment = var.environment
+	image = "ubuntu-18.04"
+  server_type="cx11"
+}
+
 resource "azurerm_dns_a_record" "hcloud_host_dns_entry" {
   name                = "prometheus"
   zone_name           = azurerm_dns_zone.openinfrastructure_dns.name
