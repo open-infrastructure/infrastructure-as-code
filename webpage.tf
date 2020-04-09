197d0f6d9594df04d7a8e660af41c5fecef122e8
Thies Mueller <tmueller@wobcom.de>
2020-04-09 11:24:21 +0200

    added staging to open-infra

--- webpage.tf
+++ webpage.tf
@@ -14,6 +14,14 @@ resource "azurerm_dns_aaaa_record" "default_www_openinfra" {
   records             = ["2a01:4f9:c010:2dba::1"]
 }
 
+resource "azurem_dns_cname_record" "staging_openinfra" {
+  name                =  "staging"
+  zone_name           = azurerm_dns_zone.openinfrastructure_dns.name
+  resource_group_name = azurerm_resource_group.openinfrastructure_rg.name
+  ttl                 = 3600
+  records             = ["www.open-infrastructure.de."]
+}
+
 resource "azurerm_dns_a_record" "www_openinfra" {
   name                = "www"
   zone_name           = azurerm_dns_zone.openinfrastructure_dns.name
