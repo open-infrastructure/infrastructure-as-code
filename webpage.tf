6798fac5e4581d154d502d650c4ef2cf85680ec7
Thies Mueller <tmueller@wobcom.de>
2020-04-09 11:25:36 +0200

    fixed typo

--- webpage.tf
+++ webpage.tf
@@ -14,7 +14,7 @@ resource "azurerm_dns_aaaa_record" "default_www_openinfra" {
   records             = ["2a01:4f9:c010:2dba::1"]
 }
 
-resource "azurem_dns_cname_record" "staging_openinfra" {
+resource "azurerm_dns_cname_record" "staging_openinfra" {
   name                =  "staging"
   zone_name           = azurerm_dns_zone.openinfrastructure_dns.name
   resource_group_name = azurerm_resource_group.openinfrastructure_rg.name
