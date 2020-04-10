908be9cf60c1adaf43b832bf844d8ebec24b5652
cedi <cedi@users.noreply.github.com>
2020-04-10 16:07:02 +0200

    fix

--- auth.tf
+++ auth.tf
@@ -4,7 +4,6 @@ module "auth" {
   ssh_keys = ["cedi@ivy", "cedi@ivy legacy", "azure_pipelines"]
   az_dns_zone = azurerm_dns_zone.openinfrastructure_dns
 	environment = var.environment
-	image = "centos-8"
 	image = "ubuntu-18.04"
   server_type = "cx21"
 }
