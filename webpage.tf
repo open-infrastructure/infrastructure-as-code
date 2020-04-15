43a205a4d8b4269488d9145636609d1b1737808c
Thies Mueller <tmueller@wobcom.de>
2020-04-15 19:11:24 +0200

    add ev

--- webpage.tf
+++ webpage.tf
@@ -22,6 +22,22 @@ resource "azurerm_dns_cname_record" "staging_openinfra" {
   record              = "www.open-infrastructure.de."
 }
 
+ resource "azurerm_dns_a_record" "ev_openinfra" {
+   name                = "ev"
+   zone_name           = azurerm_dns_zone.openinfrastructure_dns.name
+   resource_group_name = azurerm_resource_group.openinfrastructure_rg.name
+   ttl                 = 3600
+   records             = ["95.216.192.39"]
+ }
+
+ resource "azurerm_dns_aaaa_record" "ev_openinfra" {
+   name                = "ev"
+   zone_name           = azurerm_dns_zone.openinfrastructure_dns.name
+   resource_group_name = azurerm_resource_group.openinfrastructure_rg.name
+   ttl                 = 3600
+   records             = ["2a01:4f9:c010:2dba::1"]
+ }
+
 resource "azurerm_dns_a_record" "www_openinfra" {
   name                = "www"
   zone_name           = azurerm_dns_zone.openinfrastructure_dns.name
