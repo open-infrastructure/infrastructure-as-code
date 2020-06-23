554c840065e8c7d49f77acfb75ba31742ce9da2f
cedi <cedi@users.noreply.github.com>
2020-06-23 11:27:30 +0200

    add VPC subnet

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,5 +1,11 @@
-resource "hcloud_network" "privNet" {
+resource "hcloud_network" "k8_network" {
   name = "my-net"
-  ip_range = "10.240.0.0/24"
+  ip_range = "10.240.0.0/16"
 }
 
+resource "hcloud_network_subnet" "k8_subnet" {
+  network_id = hcloud_network.k8_network.id
+  type = "cloud"
+  network_zone = "eu-central"
+  ip_range   = "10.240.0.0/24"
+}
